7fbfb69aaa36fc6995b6f42f9d0cb5859ccf088a
nsixp <imamsetopo@gmail.com>
2023-05-04 08:05:32 +0700

    First Commit

---
+++ dist/js/script.js
@@ -0,0 +1,34 @@
+const loader = document.querySelector('.container-loader');
+
+function loaderActive() {
+    loader.classList.add('loader-active');
+}
+
+function loaderActiveTime() {
+    setInterval(loaderActive, 3000);
+}
+
+window.onload = loaderActiveTime();
+
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("filter-item")){
+       // deactivate existing active 'filter-item'
+       filterContainer.querySelector(".active").classList.remove("active");
+       // activate new 'filter-item'
+       event.target.classList.add("active");
+       const filterValue = event.target.getAttribute("data-filter");
+       galleryItems.forEach((item) =>{
+      if(item.classList.contains(filterValue) || filterValue === 'all'){
+          item.classList.remove("hide");
+           item.classList.add("show");
+      }
+      else{
+          item.classList.remove("show");
+          item.classList.add("hide");
+      }
+       });
+  }
+});
